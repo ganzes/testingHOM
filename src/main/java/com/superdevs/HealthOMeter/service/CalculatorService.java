463eb608364734a12106a67312d8c1c81cc56bde
Przemyslaw.Serbenski <serbenski.p@gmail.com>
2020-10-21 12:23:52 +0200

    E001 Implementation + Utest

---
+++ src/main/java/com/superdevs/HealthOMeter/service/CalculatorService.java
@@ -0,0 +1,19 @@
+package com.superdevs.HealthOMeter.service;
+
+import com.superdevs.HealthOMeter.service.managers.CalculatorManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class CalculatorService {
+
+    @Autowired
+    CalculatorManager calculatorManager;
+
+    public BigDecimal getCalculateBMI(BigDecimal weight, BigDecimal height) {
+        return calculatorManager.calculateBMI(weight, height);
+    }
+
+}
--- src/test/java/com/superdevs/HealthOMeter/HealthOMeterApplicationTests.java
+++ src/test/java/com/superdevs/HealthOMeter/HealthOMeterApplicationTests.java
@@ -1,13 +1,37 @@
 package com.superdevs.HealthOMeter;
 
+
+import com.superdevs.HealthOMeter.service.CalculatorService;
+import com.superdevs.HealthOMeter.service.managers.CalculatorManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 class HealthOMeterApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testCalculatorServiceForBMI() {
+		//given
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext(CalculatorManager.class, CalculatorService.class);
+		CalculatorService calculatorService = context.getBean(CalculatorService.class);
+		BigDecimal weight = new BigDecimal("119.522");
+		BigDecimal height = new BigDecimal("1.971");
+		BigDecimal expectedValue = new BigDecimal("30.8");
+		//when
+		BigDecimal result = calculatorService.getCalculateBMI(weight, height);
+		//then
+		Assertions.assertEquals(0, result.compareTo(expectedValue));
 	}
 
 }
